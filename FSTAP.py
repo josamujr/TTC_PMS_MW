8a944175fbd9d5fda5c7ed20da91bb6c5a2da293
josamujr <andydefloat@gmail.com>
2023-11-12 13:54:22 -0800

    raw based access control

--- FSTAP.py
+++ FSTAP.py
@@ -1,17 +1,69 @@
-from fastapi import FastAPI,Path 
+from fastapi import FastAPI, File, UploadFile, Depends, status, APIRouter, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel 
-from typing import Optional
-import dbase
+from typing import Optional, Annotated, List
 from bson.objectid import ObjectId
+from datetime import datetime,timedelta
+import dbase as db, codecs,schemas
+from dataclasses import dataclass
+from passlib.context import CryptContext
 
 
+router = APIRouter(tags=['Authentication'])
+app = FastAPI()
+authenticator = db.Authentication()
+dbase = db.DataBase_Manager()
+acc = db.Access()
 class Item(BaseModel):
     name : str
     Surname :str
     details : Optional[str]
     id : Optional[str]
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+    User : list
+
+class USer_data(BaseModel):
+    username : str
+    position : str
+    password : str
     
-app = FastAPI()
+
+class PrisonerDetails(BaseModel):
+    name : str 
+    dateOfBrth : str 
+    prisonID : int 
+    village : str
+    sentenceLength : str 
+    releaseDate : str
+    Case_file_number : int
+    medical_details : list[str]
+    
+class CaseMgmt_data(BaseModel):
+    FileNumber : int
+    involved_inmates : list
+    Offense_details : str
+    Risk_assessment : str
+    Case_mngr : Optional[str]
+ 
+class Guards_data(BaseModel):
+    name : str
+    contact : int
+    employee_id : int
+    village : Optional[str]   
+
+class MedicReport(BaseModel):
+    prisoner_id : int
+    report : list[str]
+class response(BaseModel):
+    prisoner_id : int
+    report : list[str]
+    
+class credentials(BaseModel):
+    username : str
+    password : str
+    
 data = {}
 @app.get("/")
 async def root():
@@ -22,18 +74,74 @@ async def root():
     return {"data": dbase.find()}
 
 @app.post("/insert/{id}")
-async def add_new(id : int,item : Item):
+async def add_new(id : int,item : Item, get_user : int = Depends(acc.get_current_user)):
     dbase.inserting_documents([item.name, item.Surname, item.details])
     return item
 
 @app.get("/all_data")
-async def get_all_data():
+async def get_all_data(get_user : int = Depends(acc.get_current_user)):
     docs = []
     for doc in dbase.find_zose():
         docs.append(doc)
     return {"data": docs}
 
 @app.post("/update")
-async def update(item :Item):
+async def update(item :Item, get_user : int = Depends(acc.get_current_user)):
     updated = dbase.update([item.name, item.Surname, item.details, item.id])
     return updated[1] if updated else "AN ERROR OCCURED WHILE TRYING TO UPDATE THE RECORDS"
+
+
+
+@app.post("/new_user/" )
+async def new_user(data : USer_data,get_user : int = Depends(acc.get_current_user)):
+    print(get_user)
+    if get_user == "admin":
+        New_user = dbase.create_user([data.username, data.position, data.password])  
+        return New_user if new_user else None
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='USER IS NOT AUTHORISED TO PROCEED WITH THIS OPERATION')
+
+@app.post("/new_prisoner/")
+async def insert_prisoner( file: PrisonerDetails, get_user : int = Depends(acc.get_current_user)):
+    dbase.insert_prisoner([file.name,  file.dateOfBrth, file.prisonID , file.village, file.sentenceLength ,file.releaseDate, file.medical_details])
+    return file
+
+
+@app.post("/new_case/")
+async def insert_case(file : CaseMgmt_data,get_user : int = Depends(acc.get_current_user)):
+    dbase.new_case([file.FileNumber, file.involved_inmates, file.Offense_details, file.Risk_assessment, file.Case_mngr])
+    return file 
+
+
+@app.post("/new_guard")
+async def new_guard(file : Guards_data, get_user : int = Depends(acc.get_current_user)):
+    dbase.new_guard([file.name, file.contact, file.employee_id,file.village])
+    
+@app.post("/add_picture/", response_model= None)
+def new_prisoner_picture( photo: UploadFile, file : PrisonerDetails = Depends() , user_id : int = Depends(acc.get_current_user) ):
+    dbase.insert_prisoner([file.name,  file.dateOfBrth, file.prisonID , file.village, file.sentenceLength ,file.releaseDate, photo.file.read(),file.medical_details])
+    #prisoner_info = codecs.decode(file.file.read(), "latin-1")   
+    return file
+
+@app.post("/edit_medical_report")
+async def edit_medical_report(file: MedicReport):
+    return dbase.update_prisoner_medical_records([file.prisoner_id, file.report])
+    
+
+@app.post("/login/")
+def login(user_credentials:  USer_data= Depends() ):
+    user_logged_in = dbase.find_user(user_credentials.username)
+    print(user_logged_in[2])
+
+    if user_logged_in:
+        #AUTHENTICATING THE PASSWORD
+        crypto_cont = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+        if  not crypto_cont.verify(user_credentials.password, user_logged_in[1]):
+            #raise HTTPException(status_code=status.HTTP_200_OK, detail =  "PASSWORD VERIFIED SUCCESSFULLY")
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='INVALID CREDENTIALS')
+        
+        acccess_Token = acc.create_token({"username": user_logged_in[0], "Authority": user_logged_in[2] })
+        
+        return {"TOKEN"  : acccess_Token, "Token Type" : "bearer", "User" :user_logged_in}
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='INVALID CREDENTIALS')
--- dbase.py
+++ dbase.py
@@ -1,42 +1,127 @@
 #from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
-import os, json
-from itertools import islice
 from bson.objectid import ObjectId
-#load_dotenv()
-#password = os.getenv("MONGO_PASSWORD")
-uri = f"mongodb+srv://ndeineyo:mongoacc@limbika.brqsfst.mongodb.net/?retryWrites=true&w=majority"
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from PIL import Image
+from  fastapi import Depends, status , HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+import schemas
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
-bot_db = client.Bot_db
-collection = bot_db.history
-library = bot_db.libray
-pris = bot_db.Prisoners
-# 6490804f8a5af9aee42c4f31 
+  
+  
+  
+  
+class Authentication():
+  def __init__(self):
+    self.crypto_cont = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+    #oauth_scheme = OAuth2PasswordBearer(tokenUrl= "token")
+    
+    def verify(self,plain, hashed ):
+        return self.crypto_cont.verify(plain, hashed)
+    
+    def get_pass_hash(self,password):
+        return self.crypto_cont.hash(password)
+      
+      
+class DataBase_Manager:
+  def __init__(self):
+    self.uri = f"mongodb+srv://ndeineyo:mongoacc@limbika.brqsfst.mongodb.net/?retryWrites=true&w=majority"
+    self.client = MongoClient(self.uri)
+    self.bot_db = self.client.Bot_db
+    self.user = self.bot_db.Users
+    self.pris = self.bot_db.Prisoners
+    self.cases = self.bot_db.Court_Cases
+    self.guard = self.bot_db.Guards
+    #self.medical_records = self.bot_db.Medical_Records
+    self.crypto_cont = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+  def inserting_documents(self, info):
+    return True,info if self.pris.insert_one({ "Name" : info[0],"Surname": info[1], "details" : info[2]}) else False
 
-def inserting_documents(info):
-  return True,info if pris.insert_one({ "Name" : info[0],"Surname": info[1], "details" : info[2]}) else False
+  def insert_prisoner(self, info):
+    return True if self.pris.insert_one({ "Name" : info[0],"Photo": info[6] , "Date of Birth" : info[1], "Prison ID" :info[2], "Village" :  info[3] ,"Sentence Length"  : info[4], "Release Date" : info[5], "Medical Details" : info[7] }) else False
 
-def find( chat_id = None):  
-  hist = pris.find_one({"_id": ObjectId("654d5a263d98904788854490")},  projection = {"_id": 0} )
-  if hist:
-    return hist
+  def find(self,chat_id = None):  
+    hist = self.pris.find_one({"_id": ObjectId("654d5a263d98904788854490")},  projection = {"_id": 0} )
+    if hist:
+      return hist
 
-def update(info):
-  #checking if the id is presnt in the database
-  user = find(info[0])
-  if user:
-    return pris.update_one({"_id": ObjectId(info[3])}, {"$set": {"Name": info[0], "Surname" : info[1], "details" : info[2]}}), info  
+  def update(self,info):
+    #checking if the id is presnt in the database
+    self.user = self.find(info[0])
+    if self.user:
+      return self.pris.update_one({"_id": ObjectId(info[3])}, {"$set": {"Name": info[0], "Surname" : info[1], "details" : info[2]}}), info  
+    
+  def create_user(self,details):
+    return details if self.user.insert_one({"Name" : details[0], "Position" : details[1], "Password" : self.crypto_cont.hash(details[2])}) else "User Registration failed"
+
+  def find_user(self, username):
+    self.found = self.user.find_one({"Name" : username}) 
+    return self.found["Name"],self.found["Password"], self.found["Position"] if self.found else None
+    
+    
+  def find_zose(self):
+      self.hist = self.pris.find({},  projection = {"_id": 0, "Photo" : 0})
+      if self.hist:
+          self.datas = []
+          for doc in self.hist:
+            self.datas.append(doc)
+          self.dictionary = self.datas
+          #self.dictionary[-1]["Photo"]   image
+          return self.dictionary
+
+  def new_case(self, data):
+    self.cases.insert_one({"Case File Number" : data[0], "Associated Inmates" : data[1],"Offense Details" : data[2], "Risk Assessment" : data[3], "Case Manager" : data[4]  })
+    return data
+  def new_guard(self, data):
+    self.guard.insert_one({"Name" : data[0], "Contact" : data[1], "Employee Id" : data[2], "Village" : data[3]})
+    return data
+  def update_prisoner_medical_records(self,data):
+    self.user = self.pris.find_one( {"Prison ID":int(data[0])})
+    if self.user:
+      self.prisoner = self.pris.find_one({"Prison ID" : data[0]})
+      self.medical_records = self.prisoner["Medical Details"]
+      self.pris.update_one({"Prison ID": int(data[0]) }, {"$set": {"Medical Details" :   self.medical_records + data[1]}})
+      return data[1]
+    return "Prisoner not found" 
   
 
+class Access(DataBase_Manager):
+  def __init__(self):
+    
+    
+    self.secrete_key = "dfjfhdweiou438923ygdhwbaskllw12398ue"
+    self.ALGORITHM = "HS256"
+    #ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
-def find_zose():
-    hist = pris.find({},  projection = {"_id": 0})
-    if hist:
-        #datas = []
-        #for doc in find_zose():
-        #    datas.append(doc)
-        return hist
+  def create_token(self, data : dict):
+    self.to_encode = data.copy()
+    self.expire = datetime.utcnow() + timedelta(minutes= 20)
+    print(self.expire)
+    self.to_encode.update({"exp" :self.expire})
+    self.encoded_jwt = jwt.encode(self.to_encode, self.secrete_key, algorithm= self.ALGORITHM)
+    return self.encoded_jwt
+  def verify_access_token(self, token : str, credentials_exception):
+    try:
+      self.payload = jwt.decode(token, self.secrete_key, algorithms= [self.ALGORITHM])
+      self.id : str = self.payload.get("username")
+      if self.id is None:
+          raise credentials_exception
 
-        
+      #self.token_data = schemas.TokenData(id = id)  # validating the token
+    except JWTError:
+      raise credentials_exception
+    return self.payload.get("Authority")
+
+  def get_current_user(self, token : str = Depends(OAuth2PasswordBearer(tokenUrl= "login"))):
+      self.credential_exceptopn = HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail='UN AUTHORISED ACESS', headers={"WWW--AUTHENTICATE" : "Bearer"})
+      self.TOKEN = self.verify_access_token(token, self.credential_exceptopn)
+      #self.user = self.find_user(self.TOKEN.id)
+      return self.TOKEN
+
+  
+#dbms = DataBase_Manager()
+#print(type(dbms.find_user("defloat")["Password"]))
+#cc= Access()
+#print(acc.create_token({"username": "defloat", "Authority": "Chief Founder" }))
